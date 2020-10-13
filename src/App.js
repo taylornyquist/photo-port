effdef2c2971cd84cc146feae19ff86d27fed2ce
Taylor Nyquist <tnyquist@gmail.com>
2020-10-13 18:56:23 -0500

    finished nav and about sections

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
-// import logo from './logo.svg';
-import './App.css';
+import Nav from './components/Nav';
+import About from './components/About';
+// import './App.css';
 
 function App() {
   return (
     <div>
-
+      <Nav></Nav>
+      <main>
+        <About></About>
+      </main>
     </div>
   );
 }
